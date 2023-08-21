aa480e9a2ae6deed40c0d1fced47235516ac08a2
Vasiliy Sharapov <vasiliys@gmail.com>
2023-08-21 09:31:02 -0400

    expose via loadbalancer

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ kubectl apply -f namespace.yaml
 # Apply the deployment within the namespace
 kubectl apply -f deployment.yaml -n k8s-ssl-updater
 
-echo "Deployment has been applied to the k8s-ssl-updater namespace."
+# Apply the service within the namespace
+kubectl apply -f service.yaml -n k8s-ssl-updater
+
+echo "Deployment and service have been applied to the k8s-ssl-updater namespace."
