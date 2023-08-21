302b34e343de31ea59e6cdb1f02058081a5e0633
Vasiliy Sharapov <vasiliys@gmail.com>
2023-08-21 10:59:56 -0400

    applied serviceAccount to deployment and typo fix

--- web_interface.py
+++ web_interface.py
@@ -2,7 +2,7 @@ from click_web import create_click_web_app
 import my_cli
 import kube_secrets_list
 
-app = create_click_web_app([my_cli.cli, kube_secrets_lister.cli])
+app = create_click_web_app([my_cli.cli, kube_secrets_list.cli])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
